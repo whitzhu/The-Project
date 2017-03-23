eb59161fde1eb974ad26c4679fbb1c37ad33491b
whitzhu <whitzhu@gmail.com>
2017-03-23 12:46:30 -0700

    update package.json start script

--- server/index.js
+++ server/index.js
@@ -197,7 +197,8 @@ app.post('/vision', function(req, res) {
   });
 });
 
+var port = process.env.PORT || 3000;
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log('listening on port 3000!');
 });
